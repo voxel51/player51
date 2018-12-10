374e5611184494f3c9ac321ca0cffb4e283579f4
Brandon Paris <brandon@Brandons-MBP.voxel.private>
2018-12-10 10:03:54 -0500

    Update example

--- test/react-example.js
+++ test/react-example.js
@@ -1,12 +1,12 @@
 import ReactPlayer51 from "../src/js/react-player51";
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Overlay from "./player51-test-data/8Xxvx8V-hnc-001.json";
 
 class App extends React.Component {
   render() {
     return (
-      <ReactPlayer51 src="" overlay="" fps={29} />
+      <ReactPlayer51 src="http://0.0.0.0:8000/test/player51-test-data/8Xxvx8V-hnc-001.mp4" type="video/mp4" overlay={Overlay} fps={29} />
     );
   }
 }
