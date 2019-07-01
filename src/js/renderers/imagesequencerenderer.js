52f24f81348e8734e103b58834139ba8be205c29
Kevin <kevinqi34@gmail.com>
2019-07-01 15:55:21 -0400

    more progress

--- src/js/renderers/imagesequencerenderer.js
+++ src/js/renderers/imagesequencerenderer.js
@@ -32,6 +32,94 @@ export {
  */
 function ImageSequenceRenderer(media, overlay) {
   Renderer.call(this, media, overlay);
+  // Data structures
+  this.imageFiles = {};
+  // Loading state attributes
+  this._isImageInserted = false;
+  // Initialization
+  this.openContents();
 }
 ImageSequenceRenderer.prototype = Object.create(Renderer.prototype);
 ImageSequenceRenderer.prototype.constructor = ImageSequenceRenderer;
+
+
+/**
+ * Initializes an image sequence player in parent
+ *
+ * @member initPlayer
+ * @required setParentofMedia called beforehand
+ */
+ImageSequenceRenderer.prototype.initPlayer = function() {
+  this.checkParentandMedia();
+  this.checkBorderBox();
+  this.eleDivImage = document.createElement('div');
+  this.eleDivImage.className = 'p51-contained-image';
+  this.parent.appendChild(this.eleDivImage);
+  this.initCanvas();
+};
+
+
+/**
+ * This loads controls for imagesequence
+ *
+ * @member initPlayerControls
+ * @required player to be set
+ */
+ImageSequenceRenderer.prototype.initPlayerControls = function() {
+  this.checkPlayer();
+};
+
+
+/**
+ * This function is a controller
+ * The loading state of the player has changed and various settings have to be
+ * toggled.
+ *
+ * @member updateFromLoadingState
+ */
+ImageSequenceRenderer.prototype.updateFromLoadingState = function() {
+  /*
+  if (this._isRendered) {
+    if ((this._overlayData !== null) && (this._overlayURL !== null)) {
+      this._overlayCanBePrepared = true;
+    }
+  }
+
+  if (this._overlayCanBePrepared) {
+    this.prepareOverlay(this._overlayData);
+    console.log(this);
+  }
+  */
+};
+
+
+/**
+ * Generate a string that represents the state.
+ *
+ * @member state
+ * @return {dictionary} state
+ */
+ImageSequenceRenderer.prototype.state = function() {
+  return `
+ImageSequenceRenderer State Information:
+isZipReady: ${this._isZipReady}
+isRendered:   ${this._isRendered}
+overlayCanBePrepared: ${this._overlayCanBePrepared}
+isReadyProcessFrames: ${this._isReadyProcessFrames}
+overlayCanBePrepared: ${this._overlayCanBePrepared}
+isOverlayPrepared: ${this._isOverlayPrepared}
+isPreparingOverlay: ${this._isPreparingOverlay}
+`;
+};
+
+
+/**
+ * Loads blob data into datastructures
+ *
+ * @member handleBlob
+ * @param {blob} blob
+ * @param {path} filename
+ */
+ImageSequenceRenderer.prototype.handleBlob = function(blob, filename) {
+  console.log('WHAT UP!');
+};
