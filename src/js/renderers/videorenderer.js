voxel51/player51

commit 84f51f67f58acb2ae0b39e58ddf3fb5c9739cb41

Fix and clarify FPS fallback logic

src/js/renderers/videorenderer.js
@@ -444,9 +444,12 @@ VideoRenderer.prototype.updateFromLoadingState = function() {
     this.prepareOverlay(this._overlayData);
   }
 
-  const numFrames = Object.keys(this.frameOverlay).length || 30;
   if ((!isFinite(this.frameRate) || !isFinite(this.frameDuration)) &&
-      !isNaN(this.eleVideo.duration) && this.frameOverlay) {
+      isFinite(this.eleVideo.duration)) {
+    // FPS wasn't provided, so guess it from the labels. If we don't have labels
+    // either, we can't determine anything, so fall back to FPS = 30.
+    const numFrames = Object.keys(this.frameOverlay).length ||
+        this.eleVideo.duration * 30;
     this.frameRate = numFrames / this.eleVideo.duration;
     this.frameDuration = 1 / this.frameRate;
   }

commit 09d84cbcd17b3bdb0a3421a2a62f221e0fe02d79

Fix FPS detection for slow-loading labels
84f51f67f58acb2ae0b39e58ddf3fb5c9739cb41 broke the case where labels do exist but are slow to load

src/js/renderers/videorenderer.js
@@ -446,16 +446,16 @@ VideoRenderer.prototype.updateFromLoadingState = function() {
 
   if (this._overlayCanBePrepared) {
     this.prepareOverlay(this._overlayData);
-  }
 
-  if ((!isFinite(this.frameRate) || !isFinite(this.frameDuration)) &&
-      isFinite(this.eleVideo.duration)) {
-    // FPS wasn't provided, so guess it from the labels. If we don't have labels
-    // either, we can't determine anything, so fall back to FPS = 30.
-    const numFrames = Object.keys(this.frameOverlay).length ||
-        this.eleVideo.duration * 30;
-    this.frameRate = numFrames / this.eleVideo.duration;
-    this.frameDuration = 1 / this.frameRate;
+    if ((!isFinite(this.frameRate) || !isFinite(this.frameDuration)) &&
+        isFinite(this.eleVideo.duration)) {
+      // FPS wasn't provided, so guess it from the labels. If we don't have
+      // labels either, we can't determine anything, so fall back to FPS = 30.
+      const numFrames = Object.keys(this.frameOverlay).length ||
+          this.eleVideo.duration * 30;
+      this.frameRate = numFrames / this.eleVideo.duration;
+      this.frameDuration = 1 / this.frameRate;
+    }
   }
 };
 