cb70e5708abbd8fd135a1e677bbedf85f3260f78
Benjamin Kane <ben@voxel51.com>
2020-10-09 13:19:27 -0400

    simplify _prepareOverlay_auxCheckAdd

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -543,13 +543,10 @@ Renderer.prototype._prepareOverlay_auxCheckAdd = function(o, fn = -1) {
     fn = this._frameNumber;
   }
   if (fn in this.frameOverlay) {
-    const thelist = this.frameOverlay[fn];
-    thelist.push(o);
-    this.frameOverlay[fn] = thelist;
+    this.frameOverlay[fn].push(o);
   } else {
     // this the first time we are seeing the frame
-    const newlist = [o];
-    this.frameOverlay[fn] = newlist;
+    this.frameOverlay[fn] = [o];
   }
 };
 
